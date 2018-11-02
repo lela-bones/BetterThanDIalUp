e91f6d72aaa280455cf1fde2ea4d87ff38cc6cea
Matt Matuk <coolskater6088@hotmail.com>
2018-11-02 09:42:40 -0400

    Added Freq Table cal

--- Project_1/project1.c
+++ Project_1/project1.c
@@ -14,10 +14,13 @@ int main(int argc, char *argv[])
     int *sendcounts, *displs; // Array for how many elements to send and displacement of elements.
     int *ranArry = NULL; //array of random size.
     int *localArray; // used to store random values for each process.
+    int *localFreqTable; // Store the frequency of distink values.
+    int *totalFreqTable; // store sum of freq
     int rem; // remaining elements after division among processes.
     int sum = 0; // Sum of counts. Used to calculate displacements
     int range; //Range for the random numbers
     int totalParity, sumParity;
+    double startTime = 0.0, endTime;
 
     
     MPI_Init(&argc, &argv);
@@ -34,6 +37,8 @@ int main(int argc, char *argv[])
             printf("Enter the size of the array: (0 quits) ");
             scanf("%d", &size);
         }
+        if (myid == 0)
+            startTime = MPI_Wtime();
         MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         if (size == 0)
@@ -48,6 +53,8 @@ int main(int argc, char *argv[])
             sendcounts = (int *)malloc(numprocs * sizeof(int));
             displs = (int *)malloc(numprocs * sizeof(int));
 
+            // Process 0 will create the random array and determine the size of each of the other
+            // processes, include self
             if (myid == 0)
             {
                 ranArry = (int *)malloc(size * sizeof(int)); // create random array
@@ -56,7 +63,7 @@ int main(int argc, char *argv[])
                 scanf("%d", &range);
                 
                 PopulateArray(ranArry, size, range); // add random numbers to array
-                PrintArray(ranArry, size);
+                //PrintArray(ranArry, size);
 
                 rem = size % numprocs; // remaining slots
 
@@ -73,8 +80,13 @@ int main(int argc, char *argv[])
                     displs[i] = sum;
                     sum += sendcounts[i];
                 }
+
+                totalFreqTable = (int *)calloc(range+1, sizeof(int));
             }
             MPI_Bcast(sendcounts, numprocs, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(&range, numprocs, MPI_INT, 0, MPI_COMM_WORLD);
+
+            localFreqTable = (int *)calloc(range + 1, sizeof(int));
 
             // send parts of main array to processes.
             MPI_Scatterv(ranArry, sendcounts, displs, MPI_INT, localArray, (size/numprocs) + 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -85,12 +97,23 @@ int main(int argc, char *argv[])
                 //printf ("Myid: %d, randArray index: %d, vale: %d\n", myid, i, localArray[i]);
                 if (localArray[i] % 2 == 0)
                     sumParity++;
+
+                // Increase freq.
+                localFreqTable[localArray[i]]++;
             }
-            MPI_Reduce(&sumParity, &totalParity, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-            MPI_Barrier(MPI_COMM_WORLD);
+
+            MPI_Reduce(&sumParity, &totalParity, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); // give process 0 sum of parity
+            MPI_Reduce(localFreqTable, totalFreqTable, range +1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); // gives process 0 total freq table
+            MPI_Barrier(MPI_COMM_WORLD); // Might not be needed, but just wanted to make sure all process got here before printing results
+            
+            //Display results
             if (myid == 0)
             {
-                printf("***********Number of even numbers: %d\n", totalParity);
+                endTime = MPI_Wtime();
+                printf("***Number of even numbers: %d\n", totalParity);
+                printf("***Freq Table: ");
+                PrintArray(totalFreqTable, range + 1);
+                printf("wall clock time = %f seconds\n", endTime - startTime);
             }
             
         }
@@ -98,10 +121,12 @@ int main(int argc, char *argv[])
         if (myid == 0)
         {
             free (ranArry);
+            free (totalFreqTable);
         }
         free (localArray);
         free (sendcounts);
         free (displs);
+        free (localFreqTable);
         break;
     }
 
